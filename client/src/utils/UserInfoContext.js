8e86287e5d46ad89f189a6dcead9ee6bb10ac9ca
Rachel Iaconianni <iaconiannir@gmail.com>
2020-05-02 13:31:26 -0400

    changed

--- client/src/utils/UserInfoContext.js
+++ client/src/utils/UserInfoContext.js
@@ -6,7 +6,6 @@ import React from 'react';
 const UserInfoContext = React.createContext({
   savedEntries: [],
   username: '',
-  email: '',
   journalCount: 0,
   getUserData: () => undefined,
 });
