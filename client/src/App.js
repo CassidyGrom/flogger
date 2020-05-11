e92e5bdbd5cf7b70e2de870a9ab43d576eee4070
Rachel Iaconianni <iaconiannir@gmail.com>
2020-05-11 10:06:11 -0400

    added transition

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { AnimatedSwitch, spring, AnimatedRoute  } from 'react-router-transition';
 import NewEntry from './pages/newEntries';
 import SavedEntries from './pages/savedEntries.js';
 import Navbar from './components/Navbar';
@@ -9,6 +10,42 @@ import AuthService from './utils/auth';
 
 // import our context object for state
 import UserInfoContext from './utils/UserInfoContext';
+
+// we need to map the `scale` prop we define below
+// to the transform style property
+function mapStyles(styles) {
+  return {
+    opacity: styles.opacity,
+    transform: `scale(${styles.scale})`,
+  };
+}
+
+// wrap the `spring` helper to use a bouncy config
+function bounce(val) {
+  return spring(val, {
+    stiffness: 330,
+    damping: 22,
+  });
+}
+
+// child matches will...
+const bounceTransition = {
+  // start in a transparent, upscaled state
+  atEnter: {
+    opacity: 0,
+    scale: 1,
+  },
+  // leave in a transparent, downscaled state
+  atLeave: {
+    opacity: bounce(0),
+    scale: bounce(.5),
+  },
+  // and rest at an opaque, normally-scaled state
+  atActive: {
+    opacity: bounce(2),
+    scale: bounce(1),
+  },
+};
 
 function App() {
   // set data to be used for UserInfoContext and make it available to all other components
@@ -43,11 +80,16 @@ function App() {
         {/* wrap our entire app in context provider and provide userInfo state as value */}
         <UserInfoContext.Provider value={userInfo}>
           <Navbar />
-          <Switch>
+          <AnimatedSwitch
+         atEnter={bounceTransition.atEnter}
+         atLeave={bounceTransition.atLeave}
+         atActive={bounceTransition.atActive}
+         mapStyles={mapStyles}
+         className="route-wrapper">
             <Route exact path='/' component={NewEntry} />
             <Route exact path='/saved' component={SavedEntries} />
             <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-          </Switch>
+          </AnimatedSwitch>
         </UserInfoContext.Provider>
       </>
     </Router>
